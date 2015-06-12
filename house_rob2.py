e17979bfa261c5cf92aa538903b4f5d9f58197c7
adiggo <adiggo@gmail.com>
2015-06-12 08:51:17 -0700

    only need to calculate 2n times

--- house_rob2.py
+++ house_rob2.py
@@ -9,7 +9,7 @@ class Solution:
             return nums[0]
         size = len(nums)
         max_value = max(nums[0], nums[1])
-        for i in range(0, len(nums)):
+        for i in range(0, 2):
             first = nums[i]
             #edge check
             second = max(nums[i], nums[i+1]) if i < len(nums)-1 else max(nums[i], nums[0])
