bfafa543af2c2fa91504aa966b9f968c07ef5990
xiaoping <xiaoping@talkme.im>
2015-11-23 20:24:49 +0800

    dp coin change

---
+++ dp/coin_changes/coin_change.py
@@ -0,0 +1,29 @@
+#param m: data size
+#param n: target value
+def count(data, m, n):
+    if n == 0:
+        return 1
+    if m <= 0:
+        return 0
+    if n < 0:
+        return 0
+    return count(data, m-1, n) + count(data, m, n-data[m-1])
+
+
+
+#params same as above. But this one store all results in array so we can avoid overlap problem.
+def get_num(data, m, n):
+    helper = [[0 for x in range(m)] for x in range(n + 1)]
+    # initial value set up
+    for i in range(m):
+        helper[0][i] = 1
+
+    for i in range(1, n+1):
+        for j in range(m):
+            x = helper[i-data[j]][j] if i- data[j] >= 0 else 0
+            y = helper[i][j-1] if j >=1 else 0
+            helper[i][j]=x+y
+    return helper[n][m-1]
+
+
+print get_num([1,2,3],3,4)
