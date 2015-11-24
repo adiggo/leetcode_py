2253484955b7e9a28c86fcc9eee6146ba0d85781
xiaoping <xiaoping@talkme.im>
2015-11-24 16:41:20 +0800

    add more comments

--- dp/coin_changes/coin_change.py
+++ dp/coin_changes/coin_change.py
@@ -9,7 +9,7 @@ def count(data, m, n):
         return 0
     return count(data, m-1, n) + count(data, m, n-data[m-1])
 
-
+# there are two kind of solution: one include current data, another exclude current data.
 
 #params same as above. But this one store all results in array so we can avoid overlap problem.
 def get_num(data, m, n):
@@ -20,10 +20,11 @@ def get_num(data, m, n):
 
     for i in range(1, n+1):
         for j in range(m):
+            # include current data 
             x = helper[i-data[j]][j] if i- data[j] >= 0 else 0
+            # exclude current data
             y = helper[i][j-1] if j >=1 else 0
             helper[i][j]=x+y
     return helper[n][m-1]
-
 
 print get_num([1,2,3],3,4)
