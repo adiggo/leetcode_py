fbc6b879ed8b38c6580ecff3479c871905d4aabf
adiggo <adiggo@gmail.com>
2016-01-18 16:03:22 -0800

    use constant space

--- maximum_subarray_53.py
+++ maximum_subarray_53.py
@@ -21,3 +21,29 @@ class Solution(object):
                 dp[i] = dp[i-1] + v if dp[i-1] + v > 0 else v
             max_sum = max(max_sum, dp[i])
         return max_sum
+
+
+
+
+    def maxSubArray2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums is None or len(nums) == 0:
+            return 0
+        max_sum = nums[0]
+        
+        cur= nums[0]
+        for i, v in enumerate(nums):
+            if i == 0:
+                continue
+            if v >= 0:
+                cur = cur + v if cur > 0 else v
+            else:
+                cur = cur + v if cur + v >= 0 else v
+            max_sum = max(max_sum, cur)
+        return max_sum
+
+
+
