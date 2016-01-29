1d61166cfefd9db893b8fbfa3eabd49667610686
adiggo <adiggo@gmail.com>
2016-01-28 20:10:40 -0800

    add second implementation

--- binary_tree_level_order_traversal_102.py
+++ binary_tree_level_order_traversal_102.py
@@ -21,4 +21,27 @@ class Solution(object):
             level_nodes = cur_level
             res.append([x.val for x in level_nodes])
         return res
-        
+       
+
+    
+    def levelOrder2(self, root):
+        res = []
+        if not root:
+            return res
+        res.append([root.val])
+        level_nodes = [root]
+        while level_nodes:
+            cur_level = []
+            length = len(level_nodes)
+            for i in range(length):
+                node = level_nodes[0]
+                del level_nodes[0]
+                if node.left:
+                    cur_level.append(node.left.val)
+                    level_nodes.append(node.left)
+                if node.right:
+                    cur_level.append(node.right.val)
+                    level_nodes.append(node.right)
+            if cur_level:
+                res.append(cur_level)
+        return res
