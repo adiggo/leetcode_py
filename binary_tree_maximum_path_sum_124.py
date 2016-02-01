d93e901f938872c74649c3de1b56fb66e532393a
adiggo <adiggo@gmail.com>
2016-02-01 09:54:59 -0800

    add a better approach

--- binary_tree_maximum_path_sum_124.py
+++ binary_tree_maximum_path_sum_124.py
@@ -19,3 +19,23 @@ class Solution(object):
         root_val = root.val if max_child_root < 0 else root.val + max_child_root
         side_val, root_val = root_val, max(root_val, root.val + left_root + right_root)
         return max(max_child, root_val), side_val
+
+
+
+    # better approach to store the max_sum into a variable, in the dfs return the side_val
+    def __init__(self):
+        self.max_sum = -sys.maxint - 1
+    def maxPathSum2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.dfs(root)
+        return self.max_sum
+    
+    def dfs2(self, root):
+        if not root:
+            return 0
+        left, right = self.dfs(root.left), self.dfs(root.right)
+        self.max_sum = max(self.max_sum, root.val + max(left, 0)+max(right, 0))
+        return max(root.val + max(left, 0), root.val + max(right, 0))
