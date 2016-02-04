13020f9b36f8b168de874b7e01c1348aee73621e
adiggo <adiggo@gmail.com>
2016-02-03 20:01:23 -0800

    add a clean method

--- search_insert_position_35.py
+++ search_insert_position_35.py
@@ -18,3 +18,23 @@ class Solution(object):
             elif nums[mid] >= target:
                 right = mid
         return left + 1 if nums[left] < target else left
+
+    def searchInsert2(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        left = 0
+        right = len(nums) -1
+        while left <= right:
+            mid = left + (right - left)/2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return left
