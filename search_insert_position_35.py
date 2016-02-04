61545883081029a3815065ed5d3d99dde5bedcdf
adiggo <adiggo@gmail.com>
2016-02-03 19:51:42 -0800

    bst, need to add 1 for the result if no such element found

---
+++ search_insert_position_35.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        left = 0
+        right = len(nums) -1
+        if len(nums) == 1:
+            return 1 if nums[0] < target else 0
+        while left < right:
+            mid = left + (right - left)/2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] >= target:
+                right = mid
+        return left + 1 if nums[left] < target else left
