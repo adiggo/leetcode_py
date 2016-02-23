0fe1b277aec921e8c90ebd429e7fbc908b66eda9
adiggo <adiggo@gmail.com>
2016-02-22 21:29:32 -0800

    better performance for permutation

--- permutations_46.py
+++ permutations_46.py
@@ -17,3 +17,28 @@ class Solution(object):
             cur.append(nums[j])
             self.dfs(res, cur, nums)
             cur.pop()
+
+
+
+# better approach
+    def permute1(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs2(res, [], nums, [False] * len(nums))
+        return res
+    
+    def dfs2(self, res, cur, nums, used):
+        if len(cur) == len(nums):
+            res.append(list(cur))
+        for j in range(len(nums)):
+            if used[j]:
+                continue
+            else:
+                used[j] = True
+            cur.append(nums[j])
+            self.dfs2(res, cur, nums, used)
+            used[j] = False
+            cur.pop()
