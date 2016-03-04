6957fd4dc63b1b1f3c0ef49ba2fc86ccd3d1c855
adiggo <adiggo@gmail.com>
2016-03-03 21:40:11 -0800

    sort the intervals based on the start value

---
+++ merge_intervals_56.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        if not intervals:
+            return []
+        intervals = sorted(intervals)
+        res = []
+        res.append(Interval(intervals[0].start, intervals[0].end))
+        for i in range(1, len(intervals)):
+            # starting in not None guaranteed
+            prev = res[-1]
+            cur = intervals[i]
+            if prev.end < cur.start:
+                res.append(Interval(cur.start, cur.end))
+            else:
+                # merge
+                if prev.end >= cur.end:
+                    continue
+                else:
+                    prev.end = cur.end
+        return res
