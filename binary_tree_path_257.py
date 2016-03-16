8b6115db2746a8cb6f7479e688b0925edb5c908c
adiggo <adiggo@gmail.com>
2016-03-16 00:25:24 -0700

    use same api to do recursive calls

--- binary_tree_path_257.py
+++ binary_tree_path_257.py
@@ -23,3 +23,25 @@ class Solution:
         if root.right:
             self.dfs(root.right, res, path)
             path.pop()
+
+# another solution, do not create another api
+class Solution2:
+    # @param {TreeNode} root
+    # @return {string[]}
+    def binaryTreePaths(self, root):
+        # backtracking
+        if not root:
+            return []
+        
+        if not root.left and not root.right:
+            return [str(root.val)]
+        res = []
+        if root.left:
+            left = self.binaryTreePaths(root.left)
+            for l in left:
+                res.append(str(root.val) + '->' + l)
+        if root.right:
+            right = self.binaryTreePaths(root.right)
+            for r in right:
+                res.append(str(root.val) + '->' + r)
+        return res
