62a932ebf8f5eb7e571beb80a9fec5ce8e4ba6fb
adiggo <adiggo@gmail.com>
2016-03-22 20:05:31 -0700

    path sum, recursion

---
+++ path_sum_112.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return root.val == sum
+        if self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val):
+            return True
+        return False
