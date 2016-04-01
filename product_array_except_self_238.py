43c1e6c28329e25ce4ed92c1fd576b865145323c
adiggo <adiggo@gmail.com>
2016-03-31 22:25:38 -0700

    product array with constant space

--- product_array_except_self_238.py
+++ product_array_except_self_238.py
@@ -14,3 +14,19 @@ class Solution(object):
         for i in xrange(len(nums)):
             nums[i] = helper1[i] * helper2[i]
         return nums
+
+# better approach with O(n) space
+class Solution2(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        helper = [1] * len(nums)
+        for i in xrange(1, len(nums)):
+            helper[i] = nums[i - 1] * helper[i - 1]
+        tmp = 1
+        for j in xrange(len(nums) - 2, -1, -1):
+            helper[j] = nums[j + 1] * tmp * helper[j]
+            tmp = nums[j + 1] * tmp
+        return helper
