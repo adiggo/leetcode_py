7cc7c1e9cb94950b36248df452a9ef892dc048e2
adiggo <adiggo@gmail.com>
2016-04-05 12:13:03 -0700

    h index: bst, find the lower bound of first v >= l - i

---
+++ h_index2_275.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        # find the first v larger equal than n - i
+        l = len(citations)
+        left, right = 0, l-1
+        while right >= left:
+            mid = left + (right-left)/2
+            if citations[mid] >= l-mid:
+                right = mid - 1
+            elif citations[mid] < l-mid:
+                left = mid + 1
+        return l - left
