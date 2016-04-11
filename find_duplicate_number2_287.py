a66dda9c41f91f61fe1c41461e14fbe8918b735e
adiggo <adiggo@gmail.com>
2016-04-10 23:41:59 -0700

    find duplicate number: O(32*n)

--- find_duplicate_number2_287.py
+++ find_duplicate_number2_287.py
@@ -1,3 +1,4 @@
+#O(n * logn)
 class Solution(object):
     def findDuplicate(self, nums):
         """
@@ -16,3 +17,25 @@ class Solution(object):
             else:
                 start = mid + 1
         return start
+
+#reference: https://leetcode.com/discuss/89038/o-32-n-solution-using-bit-manipulation-in-10-lines
+# O(32*n)
+class Solution2(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        start, end = 1, len(nums)-1
+        for i in xrange(32):
+            a, b = 0, 0
+            bit = 1 << i
+            for j in xrange(len(nums)):
+                if j & bit:
+                    a += 1
+                if nums[j] & bit:
+                    b += 1
+            if b > a:
+                res += bit
+        return res
