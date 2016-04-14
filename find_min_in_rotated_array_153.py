dbae3dce495361dca89cfa3a167a7e9f15f975cd
adiggo <adiggo@gmail.com>
2016-04-14 00:28:44 -0700

    clean code

--- find_min_in_rotated_array_153.py
+++ find_min_in_rotated_array_153.py
@@ -28,3 +28,26 @@ class Solution(object):
                 return min(nums[left], nums[right])
         return nums[left]
         
+
+
+class Solution2(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        left, right = 0, len(nums) -1
+        # if the first point in another location other than 0, then nums[left] > nums[right]
+        while left < right:
+            mid = left + (right - left)/2
+            # base case, mid == left
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                # not include mid
+                left = mid + 1
+        return nums[left]
