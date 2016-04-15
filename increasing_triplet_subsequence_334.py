1882e5e2b85177db6878972ef6984622bb08facd
adiggo <adiggo@gmail.com>
2016-04-14 22:59:36 -0700

    clean code

--- increasing_triplet_subsequence_334.py
+++ increasing_triplet_subsequence_334.py
@@ -29,3 +29,24 @@ class Solution(object):
                         else:
                             newFirst = nums[i]
         return False
+
+
+# reference: http://bookshadow.com/weblog/2016/02/16/leetcode-increasing-triplet-subsequence/
+class Solution2(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # record first, second
+        first, second= None, None
+        # 2, 5, 1, 10 --> if 1<a: then we can discard 2, update a = 1
+        for n in nums:
+            if first is None or first >= n:
+                first = n
+            elif second is None or second >= n:
+                second = n
+            else:
+                return True
+        return False
+                    
