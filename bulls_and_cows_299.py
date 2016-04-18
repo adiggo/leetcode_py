cc51a2a4d7f88d80f601b40b0b984f79d575bba4
adiggo <adiggo@gmail.com>
2016-04-17 23:39:06 -0700

    optimized version

--- bulls_and_cows_299.py
+++ bulls_and_cows_299.py
@@ -23,3 +23,32 @@ class Solution(object):
             b_num += min(digit_map2.get(k, 0), digit_map1.get(k, 0))
         res = res + str(b_num) + 'B'
         return res
+
+
+
+class Solution2(object):
+    def getHint(self, secret, guess):
+        """
+        :type secret: str
+        :type guess: str
+        :rtype: str
+        """
+        #check each location
+        if not secret or not guess:
+            return ''
+        bull = 0
+        result = [0] * 10
+        cow = 0
+        digit_map1 = dict()
+        digit_map2 = dict()
+        for i in xrange(len(secret)):
+            if secret[i] == guess[i]:
+                bull += 1
+            else:
+                if result[int(secret[i])] < 0:
+                    cow += 1
+                result[int(secret[i])] += 1
+                if result[int(guess[i])] > 0:
+                    cow += 1
+                result[int(guess[i])] -= 1
+        return str(bull)+ 'A' + str(cow) + 'B'
