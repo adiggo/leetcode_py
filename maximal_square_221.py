f8dd74878ece79f5938ae1d1e420dd7233092021
adiggo <adiggo@gmail.com>
2016-04-24 00:18:27 -0700

    maximal sqaure: dp

--- maximal_square_221.py
+++ maximal_square_221.py
@@ -51,4 +51,31 @@ class Solution:
             min_l = min(width,  heights[k])
             res = max(res, min_l * min_l)
         return res
-                    
+                   
+
+
+    # @param {character[][]} matrix
+    # @return {integer}
+    def maximalSquareDP(self, matrix):
+        if not matrix:
+            return 0
+        # resuse the largest historgram code
+        m, n = len(matrix), len(matrix[0])
+        res = 0
+        dp = [[0] * n for x in xrange(m)]
+        for i in xrange(m):
+            if matrix[i][0] == '1':
+                dp[i][0] = 1
+                res = max(res, dp[i][0])
+                
+        for i in xrange(n):
+            if matrix[0][i] == '1':
+                dp[0][i] = 1
+                res = max(res, dp[0][i])
+                
+        for i in xrange(1,m):
+            for j in xrange(1,n):
+                if matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+                    res = max(res, dp[i][j] * dp[i][j])
+        return res
