7470baf8b67e558b95df3b29fba9434bb387b927
adiggo <adiggo@gmail.com>
2016-05-02 22:28:18 -0700

    self crossing: search for pattern

---
+++ self_crossing_335.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isSelfCrossing(self, x):
+        for i in xrange(3, len(x)):
+            if x[i] >= x[i - 2] and x[i - 3] >= x[i - 1]:
+                return True
+            if i >= 4 and x[i - 1] == x[i - 3] and x[i] >= x[i - 2] - x[i - 4]:
+                return True
+            if i >= 5 and x[i - 2] >= x[i - 4] and x[i - 3] >= x[i - 1] and x[i - 1] >= x[i - 3] - x[i - 5] and x[i] >= \
+                            x[i - 2] - x[i - 4]:
+                return True
+        return False
