3f2fac784e217661cd712c1643f8ba6ec01dd146
adiggo <adiggo@gmail.com>
2016-05-04 23:45:06 -0700

    invert binary tree

--- invert_binary_tree_226.py
+++ invert_binary_tree_226.py
@@ -10,3 +10,18 @@ class Solution:
         root.left = self.invertTree(right)
         
         return root
+
+
+
+# add iterative via stack
+class Solution2:
+    # @param {TreeNode} root
+    # @return {TreeNode}
+    def invertTree(self, root):
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack += node.left, node.right
+        return root
