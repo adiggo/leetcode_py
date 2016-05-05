0206b530cc133f343b70d4051065d66e97c58635
adiggo <adiggo@gmail.com>
2016-05-04 22:15:34 -0700

    find min in rotated array

--- find_min_in_rotated_array_153.py
+++ find_min_in_rotated_array_153.py
@@ -51,3 +51,32 @@ class Solution2(object):
                 # not include mid
                 left = mid + 1
         return nums[left]
+
+
+
+
+class Solution3(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        left, right = 0, len(nums) -1
+        # if the first point in another location other than 0, then nums[left] > nums[right]
+        # if left == right, then we can just output nums[left]
+        while left <= right:
+            mid = left + (right - left)/2
+            # min element in range from start to mid inclusive
+            if nums[mid] < nums[right]:
+                right = mid
+            elif nums[mid] > nums[right]:
+                # min element in range from mid+1 to right
+                # not include mid because nums[mid] > nums[right]
+                # the only case for equal is that there is one element (which is excluded from while loop)
+                left = mid + 1
+            else:
+                return nums[mid]
+                
+        return nums[left]
