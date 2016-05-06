206e6da9fa1b963054c9f7fc527040e8763a4cb7
adiggo <adiggo@gmail.com>
2016-05-05 22:34:46 -0700

    pattern searching

--- integer_break_343.py
+++ integer_break_343.py
@@ -21,3 +21,32 @@ class Solution(object):
                     dp[i] = (dp[i-1] / 3) * 4
                 count += 1
         return dp[-1]
+
+
+
+class Solution2(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        #special case
+        if n == 3 or n == 2:
+            return n-1
+        count = 1
+        prev = 3
+        cur = 0
+        for i in xrange(4, n+1):
+            cur = 0
+            if i == 4:
+                cur = i
+                prev = cur
+            else:
+                if count % 3 > 0:
+                    cur = (prev / 2) * 3
+                else:
+                    cur = (prev / 3) * 4
+                count += 1
+                prev = cur
+        return cur 
+            
