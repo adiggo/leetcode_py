d6b1a1485946091cd818560f6015adba8d8c7508
adiggo <adiggo@gmail.com>
2016-05-11 23:08:58 -0700

    super ugly number. Build numbers based on a list of prime number

--- super_ugly_number_313.py
+++ super_ugly_number_313.py
@@ -18,3 +18,36 @@ class Solution(object):
                     indexes[i] += 1
             ugly_nums += m,
         return ugly_nums[-1]
+
+
+# second round: 
+# the main problem how to calculate the ugly numbers.
+# we record the index of the ugly number for each prime number
+# for dedup, it is necessary to increment the index for the corresponding prime if it equal to the min value.
+class Solution2:
+    # @param {int} n a positive integer
+    # @param {int[]} primes the given prime list
+    # @return {int} the nth super ugly number
+    def nthSuperUglyNumber(self, n, primes):
+        # Write your code here
+        if n == 1:
+            return 1
+        ugly_nums = [1]
+        indexes = [0] * len(primes)
+        
+        while len(ugly_nums) < n:
+            min_v = None
+            
+            for i in xrange(len(indexes)):
+                index = indexes[i]
+                if not min_v or ugly_nums[index] * primes[i] < min_v[0]:
+                    min_v = (ugly_nums[index] * primes[i], i)
+                    
+
+            for i in xrange(len(indexes)):
+                index = indexes[i]    
+                if ugly_nums[index] * primes[i] == min_v[0]:
+                    indexes[i] += 1
+                
+            ugly_nums += min_v[0],
+        return ugly_nums[-1]
