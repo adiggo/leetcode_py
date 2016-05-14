c9d268b2d5fc7d9d40a9e441c604f35ec059ad93
adiggo <adiggo@gmail.com>
2016-05-14 14:57:20 -0700

    min stack: record min for each position

--- min_stack_155.py
+++ min_stack_155.py
@@ -25,3 +25,57 @@ class Node:
     def __init__(self, v, min_v):
         self.v = v
         self.min_v = min_v
+
+
+
+
+
+# no need to create new class. use a list: first index store the value, second index stores the min element
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if self.stack:
+            if self.stack[-1][1] < x:
+                self.stack.append([x, self.stack[-1][1]])
+            else:
+                self.stack.append([x, x])
+        else:
+            self.stack.append([x, x])
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if not self.stack:
+            return None
+        else:
+            return self.stack.pop()[0]
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if not self.stack:
+            return None
+        else:
+            return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if not self.stack:
+            return None
+        else:
+            return self.stack[-1][1]
