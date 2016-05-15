a228e2fb44516cd94fcb500e01ceec684932d5c3
adiggo <adiggo@gmail.com>
2016-05-14 17:23:42 -0700

    merge intervals

--- merge_intervals_56.py
+++ merge_intervals_56.py
@@ -23,3 +23,43 @@ class Solution(object):
                 else:
                     prev.end = cur.end
         return res
+
+# second round
+# merge intervals
+class Solution2(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        if not intervals:
+            return []
+            
+        def mycmp(i1, i2):
+            if i1.start != i2.start:
+                return i1.start - i2.start
+            else:
+                return i1.end - i2.end
+                
+        intervals.sort(cmp=mycmp)
+        res = []
+        for i in range(len(intervals)):
+            # starting in not None guaranteed
+            if not res:
+                res.append(Interval(intervals[i].start, intervals[i].end))
+                continue
+            prev_interval = res[-1]
+            cur_interval = intervals[i]
+            if prev_interval.end < cur_interval.start:
+                res.append(Interval(cur_interval.start, cur_interval.end))
+            else:
+                # does the ending boundary equal to the starting boundary counting as same interval after merging?
+                # merge
+                
+                # case check
+                if prev_interval.end >= cur_interval.end:
+                    continue
+                else:
+                    prev_interval.end = cur_interval.end
+        return res
+                
