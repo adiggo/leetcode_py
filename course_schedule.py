8a610e3a32c49666c4d89a6cd17a7cb1b0525774
adiggo <adiggo@gmail.com>
2016-05-15 13:59:49 -0700

    course schedule: bfs: degree

--- course_schedule.py
+++ course_schedule.py
@@ -82,3 +82,43 @@ class Solution:
                 adj[courses[1]] = [courses[0]]
         return adj
 
+
+
+
+class Solution:
+    # @param {integer} numCourses
+    # @param {integer[][]} prerequisites
+    # @return {boolean}
+    def canFinish(self, numCourses, prerequisites):
+        #dfs
+        if not prerequisites or len(prerequisites) == 0:
+            return True
+        visited = [0] * numCourses
+        adj, degree = self.init_adj(prerequisites, numCourses)
+        queue = collections.deque()
+        for i in xrange(numCourses):
+            if degree[i] == 0:
+                queue.append(i)
+        while queue:
+            course = queue.popleft()
+            children = adj.get(course)
+            if children:
+                for course in adj.get(course):
+                    degree[course] -= 1
+                    if degree[course] == 0:
+                        queue.append(course)
+        for i in xrange(numCourses):
+            if degree[i] != 0:
+                return False
+        return True
+
+    def init_adj(self, prerequisites, n):
+        adj = dict()
+        degree = [0] * n
+        for courses in prerequisites:
+            if courses[1] in adj:
+                adj.get(courses[1]).append(courses[0])
+            else:
+                adj[courses[1]] = [courses[0]]
+            degree[courses[0]] += 1
+        return adj, degree
