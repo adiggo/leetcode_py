e9dc758cf431ac9d1a0de998c4b2141dfc9ef983
adiggo <adiggo@gmail.com>
2016-05-15 19:28:32 -0700

    first bad version

--- first_bad_version_278.py
+++ first_bad_version_278.py
@@ -12,3 +12,26 @@ class Solution(object):
             else:
                 left = mid + 1
         return left
+
+
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+# round 2
+class Solution2(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left <= right:
+            mid = left + (right-left)/2
+            if isBadVersion(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
