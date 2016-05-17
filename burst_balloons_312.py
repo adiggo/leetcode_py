9b00dac81c9f5921528751202b2e372feae664ed
adiggo <adiggo@gmail.com>
2016-05-16 22:21:48 -0700

    burst balloons: divide and conquer, dp

--- burst_balloons_312.py
+++ burst_balloons_312.py
@@ -19,4 +19,26 @@ class Solution(object):
         return dp[i][j]
         
         
-        
+       
+
+
+
+
+class Solution(object):
+    def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        n = len(nums)
+        nums.insert(0, 1)
+        nums.append(1)
+        dp = [[0] * (n+2) for x in xrange(n+3)]
+        for gap in xrange(n+1):
+            for i in xrange(1, n-gap+1):
+                j = i + gap
+                for k in xrange(i, i + gap + 1):
+                    dp[i][j] = max(dp[i][j], nums[i-1] * nums[k] * nums[j+1] + dp[i][k-1] + dp[k+1][j])
+        return dp[1][n]
