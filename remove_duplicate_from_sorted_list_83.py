5f1a032078432a9670cee2b32df73e732144e7d0
adiggo <adiggo@gmail.com>
2016-05-17 21:04:43 -0700

    remove duplicate from sorted list

--- remove_duplicate_from_sorted_list_83.py
+++ remove_duplicate_from_sorted_list_83.py
@@ -16,3 +16,25 @@ class Solution(object):
             else:
                 prev, cur = cur, cur.next
         return head
+
+# second round
+class Solution2(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        prev = head
+        cur = head.next
+        while cur:
+            if cur.val == prev.val:
+                prev.next = cur.next
+                cur = prev.next
+            else:
+                prev = cur
+                cur = cur.next
+        return head
+        
+        
