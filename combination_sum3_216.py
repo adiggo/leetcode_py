397187187c710b9ed2f0a46064c9ab2e79b683bc
adiggo <adiggo@gmail.com>
2016-05-17 22:48:57 -0700

    combination sum 3

--- combination_sum3_216.py
+++ combination_sum3_216.py
@@ -17,3 +17,30 @@ class Solution:
             combination.append(i)
             self.dfs(res, combination, k, n, sum+ i,i+1)
             combination.pop()
+
+# second round
+class Solution2(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(k, n, 1, [], 0, res)
+        return res
+        
+        
+    def dfs(self, k, n, index, curList, curSum, res):
+        if curSum > n:
+            return
+        if len(curList) == k:
+            if curSum == n:
+                res.append(list(curList))
+            return
+        
+        for i in xrange(index, 10):
+            curList.append(i)
+            self.dfs(k, n, i+1, curList, curSum + i, res)
+            curList.pop()
+        
