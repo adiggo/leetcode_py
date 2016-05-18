fc2200d670f6231132f1deea98cd6f796d80883d
adiggo <adiggo@gmail.com>
2016-05-18 09:04:29 -0700

    binary tree right side view

--- binary_tree_right_side_view_199.py
+++ binary_tree_right_side_view_199.py
@@ -47,3 +47,61 @@ class Solution(object):
             if level:
                 res.append(level[-1].val)
         return res
+
+# second round
+class Solution2(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if not root:
+            return res
+        self.helper(1, [0], root, res)
+        return res
+
+
+    def helper(self, depth, maxDepth, root, res):
+        if depth > maxDepth[0]:
+            maxDepth[0] = depth
+            res.append(root.val)
+        if root.right:
+            self.helper(depth + 1, maxDepth, root.right, res)
+        if root.left:
+            self.helper(depth +1, maxDepth, root.left, res)
+           
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# 1. level order traversal definitely work, but seems a waster of resource 
+# 2. optimization: traversal from right child
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if not root:
+            return res
+        self.helper(0, root, res)
+        return res
+
+
+    def helper(self, depth, root, res):
+        if not root:
+            retturn
+        if depth == len(res):
+            res.append(root.val)
+        if root.right:
+            self.helper(depth + 1, root.right, res)
+        if root.left:
+            self.helper(depth +1, root.left, res)
+            
