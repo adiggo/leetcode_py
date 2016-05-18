5167e9e1bf00604065a91f7f773cae6bf440779a
adiggo <adiggo@gmail.com>
2016-05-17 21:59:50 -0700

    paint house

--- paint_house_256.py
+++ paint_house_256.py
@@ -1,10 +1,11 @@
-def cost(costs):
-    if not costs:
-        return 0
-    r, b, g = 0, 0, 0
-    for cost in costs:
-        rr, bb, gg = r, b, g
-        r = rr + min(cost[1], cost[2])
-        b = bb + min(cost[0], cost[2])
-        g = gg + min(cost[0], cost[1])
-    return min(r, b, g)
+class Solution(object):
+    def cost(costs):
+        if not costs:
+            return 0
+        r, b, g = 0, 0, 0
+        for cost in costs:
+            rr, bb, gg = r, b, g
+            r = cost[0] + min(bb, gg)
+            b = cost[1] + min(rr, gg)
+            g = cost[2] + min(rr, bb)
+        return min(r, b, g)
