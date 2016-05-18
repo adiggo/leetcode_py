b4ee1d97146666b1554dceb20ad1628d9ff770b1
adiggo <adiggo@gmail.com>
2016-05-17 23:38:30 -0700

    merge two sorted list

--- merge_two_sorted_list_21.py
+++ merge_two_sorted_list_21.py
@@ -26,3 +26,40 @@ class Solution(object):
                 l1= l1.next if l1 is not None else None
             cur_node = cur_node.next
         return fake_node.next
+
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+# second round
+# merge two lists
+class Solution2(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        prev = dummy
+        while l1 or l2:
+            v1 = l1.val if l1 else sys.maxint
+            v2 = l2.val if l2 else sys.maxint
+            if v1 < v2:
+                prev.next = l1
+                if l1:
+                    prev = l1
+                    l1 = l1.next
+            else:
+                prev.next = l2
+                if l2:
+                    prev = l2
+                    l2 = l2.next
+                
+        return dummy.next
+            
