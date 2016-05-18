1f4c6529d3dfbfbe47d69d2010697bb842b2bb50
adiggo <adiggo@gmail.com>
2016-05-17 21:40:46 -0700

    happy number: use set to record

--- happy_number_202.py
+++ happy_number_202.py
@@ -24,3 +24,28 @@ class Solution(object):
             if s not in helper:
                 helper.add(s)
         return False
+
+
+#second round
+class Solution2(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        nums = set()
+        # once we find a cycle, check whether the cycle entry point is 1
+        while n not in nums:
+            nums.add(n)
+            n = self.get_square_num(n)
+        if n == 1:
+            return True
+        else:
+            return False
+            
+    def get_square_num(self, n):
+        res = 0
+        while n > 0:
+            res += (n % 10) ** 2
+            n /= 10
+        return res
