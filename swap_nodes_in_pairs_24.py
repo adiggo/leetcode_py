8154b6964c63462940a1d00694039ee2a5c86420
adiggo <adiggo@gmail.com>
2016-05-17 23:54:25 -0700

    swap nodes

--- swap_nodes_in_pairs_24.py
+++ swap_nodes_in_pairs_24.py
@@ -22,3 +22,40 @@ class Solution(object):
                 prev_node.next = cur_node
                 cur_node = None
         return fake_head.next
+
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# second round
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        prev = dummy
+        cur = head
+        # 1 -> 2 -> 3 -> 4
+        while cur:
+            # 2
+            next = cur.next if cur else None
+            # 3
+            nnext = next.next if next else None
+            # d->2
+            prev.next = next if next else cur
+            # d->2->1
+            if next:
+                next.next = cur
+            # break cur's previous relation
+            cur.next = None
+            # prev = 1
+            prev = cur
+            cur = nnext
+            
+        return dummy.next
