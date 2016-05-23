34c0ce92580a415b46a5f43cec8e7b12cc18d1d6
adiggo <adiggo@gmail.com>
2016-05-22 22:29:48 -0700

    house robber

--- house_robber_198.py
+++ house_robber_198.py
@@ -9,7 +9,27 @@ class Solution(object):
         cur = 0
         prev = 0
         for i in xrange(0, len(nums)):
+            # prev --> max money by nums[i-2]
+            # cur ---> max money by nums[i-1]
             tmp = prev + nums[i]
             prev = max(prev, cur)
             cur = tmp
         return max(cur, prev)
+
+
+#second round
+class Solution2(object):
+        def rob(self, nums):
+                    """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+                        return 0
+        pprev, prev = 0, 0
+        for num in nums:
+                        tmp = prev
+            prev = num + pprev
+            pprev = max(pprev, tmp)
+        
+        return max(prev, pprev)
