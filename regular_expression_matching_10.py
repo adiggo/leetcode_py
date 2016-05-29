0d1f4de934fe6d9d2ec201016d5ee7d1eec539a0
adiggo <adiggo@gmail.com>
2016-05-28 19:13:15 -0700

    dynamic programming

--- regular_expression_matching_10.py
+++ regular_expression_matching_10.py
@@ -23,4 +23,28 @@ class Solution(object):
                 else:
                     continue
         return True if l1 == l2 else False
+       
+
+
+# 
+class Solution:
+    # @return a boolean
+    def isMatch(self, s, p):
+        m, n = len(s), len(p)
+        dp=[[False for i in range(n+1)] for j in range(m+1)]
+        dp[0][0] = True
+        for i in xrange(1, n+1):
+            if i > 1 and p[i-1] == '*':
+                dp[0][i] = dp[0][i-2]
         
+        for i in xrange(1, m+1):
+            for j in xrange(1, n+1):
+                if p[j-1] != '*' and p[j-1] != '.':
+                    dp[i][j] = dp[i-1][j-1] and s[i-1] == p[j-1]
+                elif p[j-1] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = dp[i][j-1] or dp[i][j-2] or (dp[i-1][j] and (p[j-2] == s[i-1] or p[j-2] == '.' ))
+        return dp[m][n]
+                    
+                
