d74ef863fd34b5944211476bbc895c358723e176
adiggo <adiggo@gmail.com>
2016-05-29 22:35:44 -0700

    majority element 2

--- majority_element2.py
+++ majority_element2.py
@@ -21,3 +21,33 @@ class Solution:
                 c2 -= 1
         
         return [n for n in [candidate1, candidate2] if n is not None and nums.count(n) > len(nums)/3]
+
+
+# second round
+class Solution2(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # use voting algorithm
+        if not nums:
+            return []
+        n1, v1, n2, v2 = 0, None, 0, None
+        
+        for num in nums:
+            if num == v1:
+                n1 += 1
+            elif num == v2:
+                n2 += 1
+            else:
+                if n1 == 0:
+                    v1 = num
+                    n1 = 1
+                elif n2 == 0:
+                    v2 = num
+                    n2 = 1
+                else:
+                    n1 -= 1
+                    n2 -= 1
+        return [n for n in [v1, v2] if n is not None and nums.count(n) > len(nums)/3]
