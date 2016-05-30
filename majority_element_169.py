287cf9a0aa128bfc4b20aad7a9b70f42f4d70b60
adiggo <adiggo@gmail.com>
2016-05-29 22:37:57 -0700

    majority element

--- majority_element_169.py
+++ majority_element_169.py
@@ -17,3 +17,26 @@ class Solution(object):
                     maj = num
                     count = 1
         return maj
+
+# second round
+class Solution2(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        maj = 0
+        for num in nums:
+            if maj == num:
+                count += 1
+                if count > len(nums)/2:
+                    return maj
+            else:
+                if count == 0:
+                    maj = num
+                    count = 1
+                else:
+                    count -= 1
+        return maj
+        
