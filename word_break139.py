22f55ebdb9a481d5d3f9b61c4445109f97e77dfd
adiggo <adiggo@gmail.com>
2016-05-30 22:09:42 -0700

    word break

--- word_break139.py
+++ word_break139.py
@@ -14,3 +14,20 @@ class Solution(object):
                     break
         return dp[len(s)]
 
+# second round
+class Solution2(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: Set[str]
+        :rtype: bool
+        """
+        dp = [False] * (len(s) +1)
+        dp[0] = True
+        for i in xrange(len(s)):
+            for j in xrange(i+1):
+                if dp[j] and s[j:i+1] in wordDict:
+                    dp[i+1] = True
+                    break
+        return dp[len(s)]
+
