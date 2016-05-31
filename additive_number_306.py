e0602ff180db3dfa0ece7b7df3f588f98c52d8a0
adiggo <adiggo@gmail.com>
2016-05-30 17:13:15 -0700

    additive number: recursion

--- additive_number_306.py
+++ additive_number_306.py
@@ -63,3 +63,58 @@ class Solution(object):
         if num[i] == '0' and j - i == 1:
             return True
         return True if j > i else False
+
+
+# second round
+class Solution2(object):
+    def isAdditiveNumber(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        if len(num) < 3:
+            return False
+        l = len(num)
+        # each time we only need to check max(l1, l2) and max(l1, l2)+1
+        # i is the length of first num, j is the length of second num
+        for i in xrange(1, l / 2 + 1):
+            # check the number is valid
+            if self.number_valid(num, 0, i):
+                n1 = int(num[:i])
+                j = 1
+                while l - j - i >= max(i, j):
+                    if self.number_valid(num, i, i + j):
+                        n2 = int(num[i:i + j])
+                        max_l = max(i, j)
+                        n3 = [int(num[i + j: i + j + max_l]) if self.number_valid(num, i + j, i + j + max_l) else None, int(num[i + j: i + j + max_l + 1]) if self.number_valid(num, i + j, i + j + max_l + 1) else None]
+                        if n1 + n2 in n3:
+                            if n1 + n2 == n3[0] and self.is_additive(num, i + j + max_l, n2, n3[0], max_l):
+                                return True
+                            elif n1 + n2 == n3[1] and self.is_additive(num, i + j + max_l + 1, n2, n3[1], max_l + 1):
+                                return True
+                    j += 1
+
+        return False
+
+    def is_additive(self, num, cur, n1, n2, max_l):
+        # base condition
+        if cur >= len(num):
+            return True
+        
+        if cur + max_l <= len(num):
+            if self.number_valid(num, cur, cur + max_l):
+                n3 = [int(num[cur: cur + max_l]), int(num[cur: cur + max_l + 1]) if self.number_valid(num, cur, cur + max_l + 1) else None]
+                if n1 + n2 in n3:
+                    if n1 + n2 == n3[0]:
+                        return self.is_additive(num, cur + max_l, n2, n3[0], max_l)
+                    elif n1 + n2 == n3[1]:
+                        return self.is_additive(num, cur + max_l + 1, n2, n3[1], max_l + 1)
+        return False
+
+    # if a number starts with 0, then it is valid only its length is 0
+    def number_valid(self, num, i, j):
+        if num[i] == '0' and j - i > 1:
+            return False
+        if num[i] == '0' and j - i == 1:
+            return True
+        return True if j > i else False
