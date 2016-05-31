2a12b201f85a8a6f3a7de7cbe78fd125b9717c46
adiggo <adiggo@gmail.com>
2016-05-30 23:00:18 -0700

    basic calculator: a. stack  b. extract different api

--- basic_calculator2_227.py
+++ basic_calculator2_227.py
@@ -30,3 +30,58 @@ class Solution:
             else:
                 res, j = res+stack[j], j+1
         return res
+
+
+
+# second round
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def calculate(self, s):
+        stack, i = [], 0
+        while i < len(s):
+            if s[i] != ' ':
+                if s[i] == '+':
+                    i += 1
+                    d, i = self.get_digit(s, i)
+                    stack.append(d)
+                elif s[i] == '-':
+                    i += 1
+                    d, i = self.get_digit(s, i)
+                    stack.append(-d)
+                elif s[i] == '*' or s[i] == '/':
+                    op = s[i]
+                    i += 1
+                    d, i = self.get_digit(s, i)
+                    d = self.arithmetic(stack.pop(), d, op)
+                    stack.append(d)
+                else:
+                    # the first number
+                    d, i = self.get_digit(s, i)
+                    stack.append(d)
+            else:
+                i += 1
+        res = 0
+        while stack:
+            res += stack.pop()
+        return res
+        
+    def arithmetic(self, n1, n2, op):
+        if op == '*':
+            return n1 * n2
+        elif op == '/':
+            res = n1 / n2
+            # python division always floor.
+            return res if res >= 0 else int(math.ceil(float(n1)/n2))
+        else:
+            raise Exception('operator other than * and / is not allowed')
+    
+    def get_digit(self, s, s_i):
+        while s[s_i] == ' ':
+            s_i += 1
+        d = 0
+        while s_i < len(s) and s[s_i].isdigit():
+            d = 10 * d + int(s[s_i])
+            s_i += 1
+        return d, s_i
+        
