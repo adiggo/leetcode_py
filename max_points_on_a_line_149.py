e436c10793332890af43868fd7f2f8510cfdd808
adiggo <adiggo@gmail.com>
2016-06-06 08:01:05 -0700

    max points on a line: use gcd instead of directly computing slope

--- max_points_on_a_line_149.py
+++ max_points_on_a_line_149.py
@@ -33,3 +33,48 @@ class Solution(object):
         if p1.x == p2.x and p1.y == p2.y:
             return 0,1
         return float(p2.y - p1.y)/float(p2.x - p1.x) , 3
+
+
+
+# Definition for a point.
+# class Point:
+#     def __init__(self, a=0, b=0):
+#         self.x = a
+#         self.y = b
+class Solution2:
+    # @param {int[]} points an array of point
+    # @return {int} an integer
+    def maxPoints(self, points):
+        # Write your code here
+        if not points:
+            return 0
+        l = len(points)
+        res = 0
+        for i in xrange(l):
+            p1 = points[i]
+            infinite_slope = 1
+            dup = 0
+            slope_map = dict()
+            for j in xrange(i+1, l):
+                p2 = points[j]
+                x_diff = p2.x - p1.x
+                y_diff = p2.y - p1.y
+                if x_diff == 0 and y_diff == 0:
+                    dup += 1
+                elif x_diff == 0:
+                    infinite_slope += 1
+                else:
+                    divisor = self.gcd(y_diff, x_diff)
+                    slope = (y_diff/divisor, x_diff/divisor)
+                    slope_map[slope] = slope_map.get(slope, 1) + 1
+            for k in slope_map:
+                res = max(slope_map.get(k) + dup, res)
+            res = max(res, infinite_slope + dup)
+            
+        return res
+        
+        
+    def gcd(self, a, b):
+        if b == 0:
+            return a
+        return self.gcd(b, a%b)
